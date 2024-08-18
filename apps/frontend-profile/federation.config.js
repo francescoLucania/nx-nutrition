734030320c405f0675e785014b601ea00448417c
francescoLucania <35537086+francescoLucania@users.noreply.github.com>
2024-08-18 12:02:36 +0300

    init native federation (full)

---
+++ apps/frontend-profile/federation.config.js
@@ -0,0 +1,36 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  name: 'frontend-profile',
+
+  exposes: {
+    './Component': './apps/frontend-profile/src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+    'bcrypt',
+    'bcryptjs',
+    'http',
+    'axios',
+    'crypto',
+    'jsonwebtoken',
+    'nodemailer',
+    'mongoose',
+    '@nestjs',
+    'nestjs-form-data'
+    // Add further packages you don't need at runtime
+  ]
+
+  // Please read our FAQ about sharing libs:
+  // https://shorturl.at/jmzH0
+
+});
---
+++ apps/frontend/federation.config.js
@@ -0,0 +1,31 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+    'bcrypt',
+    'bcryptjs',
+    'http',
+    'axios',
+    'crypto',
+    'jsonwebtoken',
+    'nodemailer',
+    'mongoose',
+    '@nestjs',
+    'nestjs-form-data'
+    // Add further packages you don't need at runtime
+  ]
+
+
+  // Please read our FAQ about sharing libs:
+  // https://shorturl.at/jmzH0
+
+});
